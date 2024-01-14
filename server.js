99de6481ab47e6acd00a139be24ece36e710b09f
Eric w <none>
2024-01-13 19:20:44 -0800

    adding first route '/'

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
+
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Me');
-});
-
 const port = process.env.port || 3000;
 
-app.listen(port);
-console.log('Web Server is listening at port ' + port);
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+    console.log(`Web Server is listening at port ${port}`);
+});
